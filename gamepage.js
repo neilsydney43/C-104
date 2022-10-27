dc4ad8b490be03cbb9c0e2cd3e170a2c6a40393c
neilsydney43 <101984283+neilsydney43@users.noreply.github.com>
2022-10-26 20:13:19 -0400

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,31 @@
+// random value generated
+  var y=Math.floor(Math.random()*10+1);
+playername=localStorage.getItem("player_name");
+  var guess=1;
+// counting the number of guesses
+function submit()
+{
+    var x=document.getElementById("guessField").value;
+
+    if(x==y)
+    {
+        alert("CONGRATULATIONS!! "+playername+" YOU GUESSED IT RIGHT IN "+guess+" GUESSES");
+        guess=1;
+    }
+    else if(x>y)
+        {
+            guess++;
+            alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+        }
+        else
+        {
+            guess++;
+            alert("OOPS SORRY!! TRY A GREATER NUMBER");
+        }
+}
+// made for correct Guess
+function playAgain(){
+    y=Math.floor(Math.random()*10+1)
+}
+  
+// function for number guessed by user     
